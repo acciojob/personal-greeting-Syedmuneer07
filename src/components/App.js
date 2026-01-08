cc4c07c294efe9bfac2fca4fe779cf0ef2aed92a
Syed muneerul zakireen <syedmuneerzakk@gmail.com>
2026-01-08 11:08:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,19 @@
-import React, { useState } from "react";
+import React,{useState} from "react";
+import './../styles/App.css';
 
 const App = () => {
-  const [name, setName] = useState("");
-
+  const [name,setName ] = useState('')
   return (
-    <div style={{ margin: "40px" }}>
-      <label style={{ display: "block", marginBottom: "8px" }}>
-        Enter your name:
-      </label>
+   <div>
+     <p>Enter your name:</p>
+     <input
+       type="text"
+       value={name}
+       onChange={(e) => setName(e.target.value)}
+     />
+     {name ? <p>Hello {name}!</p> : null}
+   </div>
+  )
+}
 
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        style={{
-          width: "250px",
-          height: "28px",
-          padding: "4px",
-          fontSize: "14px",
-        }}
-      />
-
-      {name && (
-        <p style={{ marginTop: "15px" }}>
-          Hello, {name}!
-        </p>
-      )}
-    </div>
-  );
-};
-
-export default App;
+export default App
